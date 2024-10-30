eca4fb9b122b5dd9857c2aa959085b77a173f8b5
Thomas Nagy Zambo <thomas@ceres-space.com>
2024-10-30 20:11:49 +0100

    Kinematics done

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,19 @@
-use na::{
-    DualQuaternion, Quaternion, SMatrix, SVector, UnitDualQuaternion, UnitQuaternion, Vector3,
-};
+use na::{DualQuaternion, Quaternion, Vector3};
 use nalgebra as na;
 use re_viewer::external::{eframe, egui};
 
 mod state;
 
-use state::State;
+use state::{dq_exp, State};
 
 pub struct App {
     rerun_app: re_viewer::App,
-    roll: f32,
-    pitch: f32,
-    yaw: f32,
-    w: SVector<f32, 3>,
-    t: SVector<f32, 3>,
-    tdot: SVector<f32, 3>,
+    roll: f64,
+    pitch: f64,
+    yaw: f64,
+    omega: Vector3<f64>,
+    position: Vector3<f64>,
+    velocity: Vector3<f64>,
 }
 
 impl App {
@@ -25,9 +23,9 @@ impl App {
             roll: 0.0,
             pitch: 0.0,
             yaw: 0.0,
-            w: SVector::<f32, 3>::zeros(),
-            t: SVector::<f32, 3>::zeros(),
-            tdot: SVector::<f32, 3>::zeros(),
+            omega: Vector3::<f64>::zeros(),
+            position: Vector3::<f64>::zeros(),
+            velocity: Vector3::<f64>::zeros(),
         }
     }
 
@@ -39,27 +37,60 @@ impl App {
         let dt = 0.01;
 
         let mut state = State::from_initial_conditions(
-            &self.t,
-            &self.tdot,
+            &self.position,
+            &self.velocity,
             &self.roll,
             &self.pitch,
             &self.yaw,
-            &self.w,
+            &self.omega,
         );
 
-        re_viewer::external::re_log::debug!("{:?}", state);
+        rec.log_static(
+            "3d/drone",
+            &rerun::Asset3D::from_file("assets/drone.stl").unwrap(),
+        )
+        .unwrap();
+        rec.log_static(
+            "3d/interial_frame",
+            &rerun::Arrows3D::from_vectors(&[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+                .with_origins(&[[0.0, 0.0, 0.0]])
+                .with_colors([
+                    rerun::Color::from_rgb(255, 0, 0),
+                    rerun::Color::from_rgb(0, 255, 0),
+                    rerun::Color::from_rgb(0, 0, 255),
+                ]),
+        )
+        .unwrap();
 
         for i in 0..1000 {
-            plot_3d(i as f32 * dt, &state, &rec);
+            state.log(&rec, i as f64 * dt);
 
-            let q_dot = 0.5 * state.eta * state.q.dual_quaternion();
-            state.q = UnitDualQuaternion::new_normalize(state.q.dual_quaternion() + dt * q_dot);
-
-            let eta_dot = DualQuaternion::from_real_and_dual(
+            let f = DualQuaternion::from_real_and_dual(
                 Quaternion::new(0.0, 0.0, 0.0, 0.0),
-                Quaternion::from_imag(state.velocity().cross(&state.angular_velocity())),
+                Quaternion::from_imag(state.angular_velocity_body().cross(&state.velocity_body())),
             );
-            state.eta = state.eta + dt * eta_dot;
+
+            let f_coriolis = DualQuaternion::from_real_and_dual(
+                Quaternion::new(0.0, 0.0, 0.0, 0.0),
+                Quaternion::from_imag(
+                    -2.0 * state.angular_velocity_body().cross(&state.velocity_body()),
+                ),
+            );
+
+            let f_centrifugal = DualQuaternion::from_real_and_dual(
+                Quaternion::new(0.0, 0.0, 0.0, 0.0),
+                Quaternion::from_imag(
+                    -state
+                        .angular_velocity_body()
+                        .cross(&state.angular_velocity_body().cross(&state.position_body())),
+                ),
+            );
+
+            // eta is not constrained by the unit norm
+            state.eta = state.eta + dt * (f + f_coriolis + f_centrifugal);
+
+            // Exponential intergration to maintain unit norm of q
+            state.q = state.q * dq_exp(0.5 * dt * state.eta);
         }
     }
 }
@@ -76,82 +107,69 @@ impl eframe::App for App {
         egui::SidePanel::right("my_side_panel")
             .default_width(200.0)
             .show(ctx, |ui| {
-                ui.label("Roll");
-                ui.add(egui::DragValue::new(&mut self.roll).speed(0.1));
-                ui.label("Pitch");
-                ui.add(egui::DragValue::new(&mut self.pitch).speed(0.1));
-                ui.label("Yaw");
-                ui.add(egui::DragValue::new(&mut self.yaw).speed(0.1));
-                ui.label("Omega_x");
-                ui.add(egui::DragValue::new(&mut self.w[0]).speed(0.1));
-                ui.label("Omega_y");
-                ui.add(egui::DragValue::new(&mut self.w[1]).speed(0.1));
-                ui.label("Omega_z");
-                ui.add(egui::DragValue::new(&mut self.w[2]).speed(0.1));
-                ui.label("t_x");
-                ui.add(egui::DragValue::new(&mut self.t[0]).speed(0.1));
-                ui.label("t_y");
-                ui.add(egui::DragValue::new(&mut self.t[1]).speed(0.1));
-                ui.label("t_z");
-                ui.add(egui::DragValue::new(&mut self.t[2]).speed(0.1));
-                ui.label("tdot_x");
-                ui.add(egui::DragValue::new(&mut self.tdot[0]).speed(0.1));
-                ui.label("tdot_y");
-                ui.add(egui::DragValue::new(&mut self.tdot[1]).speed(0.1));
-                ui.label("tdot_z");
-                ui.add(egui::DragValue::new(&mut self.tdot[2]).speed(0.1));
+                ui.heading("Initial Conditions");
+                egui::Grid::new("initial_state")
+                    .num_columns(2)
+                    .spacing([20.0, 4.0])
+                    .striped(true)
+                    .show(ui, |ui| {
+                        ui.heading("Body Attitude");
+                        ui.end_row();
+                        ui.label("φ");
+                        ui.add(egui::DragValue::new(&mut self.roll).speed(0.1));
+                        ui.end_row();
+                        ui.label("θ");
+                        ui.add(egui::DragValue::new(&mut self.pitch).speed(0.1));
+                        ui.end_row();
+                        ui.label("ψ");
+                        ui.add(egui::DragValue::new(&mut self.yaw).speed(0.1));
+                        ui.end_row();
+                        ui.heading("Body Rates");
+                        ui.end_row();
+                        ui.label("p");
+                        ui.add(egui::DragValue::new(&mut self.omega[0]).speed(0.1));
+                        ui.end_row();
+                        ui.label("q");
+                        ui.add(egui::DragValue::new(&mut self.omega[1]).speed(0.1));
+                        ui.end_row();
+                        ui.label("r");
+                        ui.add(egui::DragValue::new(&mut self.omega[2]).speed(0.1));
+                        ui.end_row();
+                        ui.heading("Inertial Position");
+                        ui.end_row();
+                        ui.label("x");
+                        ui.add(egui::DragValue::new(&mut self.position[0]).speed(0.1));
+                        ui.end_row();
+                        ui.label("y");
+                        ui.add(egui::DragValue::new(&mut self.position[1]).speed(0.1));
+                        ui.end_row();
+                        ui.label("z");
+                        ui.add(egui::DragValue::new(&mut self.position[2]).speed(0.1));
+                        ui.end_row();
+                        ui.heading("Inertial Velocity");
+                        ui.end_row();
+                        ui.label("x");
+                        ui.add(egui::DragValue::new(&mut self.velocity[0]).speed(0.1));
+                        ui.end_row();
+                        ui.label("y");
+                        ui.add(egui::DragValue::new(&mut self.velocity[1]).speed(0.1));
+                        ui.end_row();
+                        ui.label("z");
+                        ui.add(egui::DragValue::new(&mut self.velocity[2]).speed(0.1));
+                        ui.end_row();
+                    });
 
-                if ui.button("Simulate").clicked() {
-                    self.simulate(ctx.clone());
-                }
+                ui.with_layout(
+                    egui::Layout::centered_and_justified(egui::Direction::LeftToRight),
+                    |ui| {
+                        if ui.button("Simulate").clicked() {
+                            self.simulate(ctx.clone());
+                        }
+                    },
+                )
             });
 
         // Now show the Rerun Viewer in the remaining space:
         self.rerun_app.update(ctx, frame);
     }
-}
-fn plot_3d(time: f32, q: &State<f32>, rec: &rerun::RecordingStream) {
-    //let t = q.translation().vector; // Body frame origin expressed in inertial frame
-    let t = q.position();
-    let tdot = q.velocity();
-    let tdot: [f32; 3] = std::array::from_fn(|i| tdot[i] as f32);
-    let t_neg = -t; // Body frame origin expressed in inertial frame
-    let t: [f32; 3] = std::array::from_fn(|i| t[i] as f32);
-    let t_neg: [f32; 3] = std::array::from_fn(|i| t_neg[i] as f32);
-    let x = q.rotation() * Vector3::x_axis(); // Body frame x-axis expressed in inertial frame
-    let x: [f32; 3] = std::array::from_fn(|i| x[i] as f32);
-    let y = q.rotation() * Vector3::y_axis(); // Body frame y-axis expressed in inertial frame
-    let y: [f32; 3] = std::array::from_fn(|i| y[i] as f32);
-    let z = q.rotation() * Vector3::z_axis(); // Body frame z-axis expressed in inertial frame
-    let z: [f32; 3] = std::array::from_fn(|i| z[i] as f32);
-    rec.set_time_seconds("sim_time", time);
-    rec.log(
-        "3d",
-        &rerun::Arrows3D::from_vectors([
-            rerun::Vector3D::from(x),
-            rerun::Vector3D::from(y),
-            rerun::Vector3D::from(z),
-            rerun::Vector3D::from(t_neg),
-            rerun::Vector3D::from(tdot),
-        ])
-        .with_origins([rerun::Position3D::from(t)])
-        .with_colors([
-            rerun::Color::from_rgb(255, 0, 0),
-            rerun::Color::from_rgb(0, 255, 0),
-            rerun::Color::from_rgb(0, 0, 255),
-            rerun::Color::from_rgb(255, 255, 0),
-            rerun::Color::from_rgb(0, 255, 255),
-        ]),
-    )
-    .unwrap();
-    let q = q.rotation();
-    let q = q.as_vector(); // Body frame origin expressed in inertial frame
-    let q: [f32; 4] = std::array::from_fn(|i| q[i] as f32);
-    rec.log(
-        "box",
-        &rerun::Boxes3D::from_centers_and_sizes([rerun::Vec3D::from(t)], [(1.0, 1.0, 0.1)])
-            .with_colors([rerun::Color::from_rgb(255, 255, 255)])
-            .with_quaternions([rerun::Quaternion::from_xyzw(q)]),
-    )
-    .unwrap();
 }
--- src/app/state.rs
+++ src/app/state.rs
@@ -1,49 +1,203 @@
-use na::{DualQuaternion, Quaternion, RealField, SVector, UnitDualQuaternion, UnitQuaternion};
+use na::{DualQuaternion, Quaternion, UnitDualQuaternion, UnitQuaternion, Vector3};
 use nalgebra as na;
 
-#[derive(Debug)]
-pub struct State<T: RealField + Copy> {
-    pub q: UnitDualQuaternion<T>,
-    pub eta: DualQuaternion<T>,
+const ROTOR_ARM1: Vector3<f64> = Vector3::<f64>::new(0.1, 0.1, 0.0);
+const ROTOR_ARM2: Vector3<f64> = Vector3::<f64>::new(0.1, -0.1, 0.0);
+const ROTOR_ARM3: Vector3<f64> = Vector3::<f64>::new(-0.1, 0.1, 0.0);
+const ROTOR_ARM4: Vector3<f64> = Vector3::<f64>::new(-0.1, -0.1, 0.0);
+
+pub fn dq_exp(q: DualQuaternion<f64>) -> UnitDualQuaternion<f64> {
+    UnitDualQuaternion::new_unchecked(DualQuaternion::from_real_and_dual(
+        q.real.exp(),
+        q.real.exp() * q.dual.clone(),
+    ))
 }
 
-impl<T: RealField + Copy> State<T> {
-    pub fn from_initial_conditions(
-        position: &SVector<T, 3>,
-        velocity: &SVector<T, 3>,
-        roll: &T,
-        pitch: &T,
-        yaw: &T,
-        omega: &SVector<T, 3>,
-    ) -> Self {
-        let q = UnitDualQuaternion::new_normalize(DualQuaternion::from_real_and_dual(
-            *UnitQuaternion::from_euler_angles(*roll, *pitch, *yaw).quaternion(),
-            *UnitQuaternion::from_euler_angles(*roll, *pitch, *yaw).quaternion()
-                * Quaternion::from_imag(*position)
-                * T::from_f32(0.5).unwrap(),
-        ));
+//pub fn cross(self, b: DualQuaternion<T>) -> DualQuaternion<T> {
+//    Self((self.clone() * b.clone() - b.conjugate() * self.conjugate()) * 0.5.into())
+//}
 
+#[derive(Clone, Debug, Default)]
+pub struct State {
+    pub q: UnitDualQuaternion<f64>,
+    pub eta: DualQuaternion<f64>,
+}
+
+impl State {
+    pub fn from_initial_conditions(
+        position: &Vector3<f64>,
+        velocity: &Vector3<f64>,
+        roll: &f64,
+        pitch: &f64,
+        yaw: &f64,
+        omega: &Vector3<f64>,
+    ) -> Self {
+        // Inital pose
+        let q = UnitDualQuaternion::from_parts(
+            (*position).into(),
+            UnitQuaternion::from_euler_angles(*roll, *pitch, *yaw),
+        );
+
+        // Initial twist
         let eta = DualQuaternion::from_real_and_dual(
             Quaternion::from_imag(*omega),
-            Quaternion::from_imag(velocity + position.cross(&omega)),
+            Quaternion::from_imag(q.rotation().conjugate() * velocity),
         );
 
         Self { q, eta }
     }
 
-    pub fn rotation(&self) -> UnitQuaternion<T> {
-        UnitQuaternion::from_quaternion(self.q.real)
+    pub fn rotation(&self) -> UnitQuaternion<f64> {
+        self.q.rotation()
     }
-
-    pub fn position(&self) -> SVector<T, 3> {
-        ((self.q.dual * self.q.real.conjugate()) * T::from_f32(2.0).unwrap()).imag()
-    }
-
-    pub fn angular_velocity(&self) -> SVector<T, 3> {
+    pub fn angular_velocity_body(&self) -> Vector3<f64> {
         self.eta.real.imag()
     }
 
-    pub fn velocity(&self) -> SVector<T, 3> {
-        self.eta.dual.imag() - self.position().cross(&self.angular_velocity())
+    pub fn position_body(&self) -> Vector3<f64> {
+        self.q.translation().vector
+    }
+
+    pub fn velocity_body(&self) -> Vector3<f64> {
+        self.eta.dual.imag() - self.angular_velocity_body().cross(&self.position_body())
+    }
+
+    pub fn position(&self) -> Vector3<f64> {
+        (self.q.dual * self.q.real.conjugate() * 2.0).imag()
+    }
+
+    pub fn velocity(&self) -> Vector3<f64> {
+        let eta = (&self.q * self.eta) * self.q.conjugate();
+        eta.dual.imag() - self.position().cross(&eta.real.imag())
+    }
+
+    pub fn log(&self, rec: &rerun::RecordingStream, t: f64) {
+        // Set timestep
+        rec.set_time_seconds("sim_time", t);
+
+        // Calcs
+        let q = self.rotation(); // Body attitude
+        let r = self.position(); // Body position expressed in body frame
+
+        // Plot drone
+        rec.log(
+            "3d/drone",
+            &rerun::InstancePoses3D::new()
+                .with_quaternions([q.into_rerun_quaternion()])
+                .with_translations([r.into_rerun_vec3d()]),
+        )
+        .unwrap();
+
+        // Plot body frame
+        let x = q * Vector3::<f64>::x_axis().into_inner() * 0.1; // Body frame x-axis expressed in inertial frame
+        let y = q * Vector3::<f64>::y_axis().into_inner() * 0.1; // Body frame y-axis expressed in inertial frame
+        let z = q * Vector3::<f64>::z_axis().into_inner() * 0.1; // Body frame z-axis expressed in inertial frame
+        rec.log(
+            "3d/body_frame",
+            &rerun::Arrows3D::from_vectors([
+                x.into_rerun_vec3d(),
+                y.into_rerun_vec3d(),
+                z.into_rerun_vec3d(),
+            ])
+            .with_origins([r.into_rerun_vec3d()])
+            .with_colors([
+                rerun::Color::from_rgb(255, 0, 0),
+                rerun::Color::from_rgb(0, 255, 0),
+                rerun::Color::from_rgb(0, 0, 255),
+            ]),
+        )
+        .unwrap();
+
+        // Plot rotor forces
+        let rotor_force1 = (q * Vector3::<f64>::z_axis().into_inner()) * 0.1;
+        let rotor_force2 = (q * Vector3::<f64>::z_axis().into_inner()) * 0.1;
+        let rotor_force3 = (q * Vector3::<f64>::z_axis().into_inner()) * 0.1;
+        let rotor_force4 = (q * Vector3::<f64>::z_axis().into_inner()) * 0.1;
+        rec.log(
+            "3d/rotor_force",
+            &rerun::Arrows3D::from_vectors([
+                rotor_force1.into_rerun_vec3d(),
+                rotor_force2.into_rerun_vec3d(),
+                rotor_force3.into_rerun_vec3d(),
+                rotor_force4.into_rerun_vec3d(),
+            ])
+            .with_origins([
+                (r + q * ROTOR_ARM1).into_rerun_vec3d(),
+                (r + q * ROTOR_ARM2).into_rerun_vec3d(),
+                (r + q * ROTOR_ARM3).into_rerun_vec3d(),
+                (r + q * ROTOR_ARM4).into_rerun_vec3d(),
+            ])
+            .with_colors([rerun::Color::from_rgb(255, 100, 100)]),
+        )
+        .unwrap();
+
+        // Log attitude
+        let (roll, pitch, yaw) = q.euler_angles();
+        rec.log("attitude/roll", &rerun::Scalar::new(roll)).unwrap();
+        rec.log("attitude/pitch", &rerun::Scalar::new(pitch))
+            .unwrap();
+        rec.log("attitude/yaw", &rerun::Scalar::new(yaw)).unwrap();
+
+        // Log angular velocity (body frame)
+        let rate = self.angular_velocity_body();
+        rec.log("omega/roll", &rerun::Scalar::new(rate[0])).unwrap();
+        rec.log("omega/pitch", &rerun::Scalar::new(rate[1]))
+            .unwrap();
+        rec.log("omega/yaw", &rerun::Scalar::new(rate[2])).unwrap();
+
+        // Log position (body frame)
+        let r_body = self.position_body();
+        rec.log("position/body/x", &rerun::Scalar::new(r_body[0]))
+            .unwrap();
+        rec.log("position/body/y", &rerun::Scalar::new(r_body[1]))
+            .unwrap();
+        rec.log("position/body/z", &rerun::Scalar::new(r_body[2]))
+            .unwrap();
+
+        // Log position (inertial frame)
+        rec.log("position/inertial/x", &rerun::Scalar::new(r[0]))
+            .unwrap();
+        rec.log("position/inertial/y", &rerun::Scalar::new(r[1]))
+            .unwrap();
+        rec.log("position/inertial/z", &rerun::Scalar::new(r[2]))
+            .unwrap();
+
+        // Log velocity (body frame)
+        let v = self.velocity_body();
+        rec.log("velocity/body/x", &rerun::Scalar::new(v[0]))
+            .unwrap();
+        rec.log("velocity/body/y", &rerun::Scalar::new(v[1]))
+            .unwrap();
+        rec.log("velocity/body/z", &rerun::Scalar::new(v[2]))
+            .unwrap();
+
+        // Log velocity (inertial frame)
+        let v = self.velocity();
+        rec.log("velocity/inertial/x", &rerun::Scalar::new(v[0]))
+            .unwrap();
+        rec.log("velocity/inertial/y", &rerun::Scalar::new(v[1]))
+            .unwrap();
+        rec.log("velocity/inertial/z", &rerun::Scalar::new(v[2]))
+            .unwrap();
+    }
+}
+
+trait IntoRerunQuaternion {
+    fn into_rerun_quaternion(&self) -> rerun::Quaternion;
+}
+
+impl IntoRerunQuaternion for Quaternion<f64> {
+    fn into_rerun_quaternion(&self) -> rerun::Quaternion {
+        rerun::Quaternion::from_xyzw(std::array::from_fn(|i| self[i] as f32))
+    }
+}
+
+trait IntoRerunVec3D {
+    fn into_rerun_vec3d(&self) -> rerun::Vec3D;
+}
+
+impl IntoRerunVec3D for Vector3<f64> {
+    fn into_rerun_vec3d(&self) -> rerun::Vec3D {
+        rerun::Vec3D::from(std::array::from_fn(|i| self[i] as f32))
     }
 }
